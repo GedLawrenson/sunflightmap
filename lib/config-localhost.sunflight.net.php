6c470e8de781314ba71266f573c62b0a50efa87f
Ged Lawrenson <gedlawrenson@Geds-MacBook-Pro.local>
2016-04-10 20:43:40 +0100

    Removed absolute file paths

--- lib/config-localhost.sunflight.net.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$cfg = array();
-$cfg["SITE_NAME"] = "Sun Flight Map";
-$cfg["PHP_TIMEZONE_OFFSET"] = 10; // Sydney GMT+10
-$cfg["SFCALC_CMD"] = "/Users/ian/Documents/projects/sunflight/sfcalc/sfcalc.py";
-$cfg['CACHE_STORAGE_PATH'] = "/Users/ian/Documents/Projects/sunflight/lib/cache/"; // files/
-$cfg['CACHE_STORAGE_KEEP_HOURS'] = (24 * 7); // 7 days
-?>
