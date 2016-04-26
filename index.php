bd16480e0f8bd2d95be8fc13ce6497a0930a27de
Ged Lawrenson <GedLawrenson@users.noreply.github.com>
2016-04-26 17:31:27 +0100

    Get jQueryRotate and Richmarker Libraries from CDN

--- index.php
+++ index.php
@@ -53,8 +53,8 @@ if(array_key_exists("autoload", $_GET)) {
 	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script src="https://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
 	<script type="text/javascript" src="/js/daynightmaptype.js"></script>
-	<script type="text/javascript" src="/js/jQueryRotate.2.2.js"></script>
-	<script type="text/javascript" src="/js/richmarker-compiled.js"></script>
+	<script type="text/javascript" src="https://raw.githubusercontent.com/wilq32/jqueryrotate/master/jQueryRotate.js"></script>
+	<script type="text/javascript" src="https://raw.githubusercontent.com/googlemaps/js-rich-marker/gh-pages/src/richmarker-compiled.js"></script>
 
 	<script type="text/javascript">
 
