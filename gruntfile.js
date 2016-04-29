49b1551a5e5b39a87d7e698edbc0feb7535d870e
Ged Lawrenson <gedlawrenson@users.noreply.github.com>
2016-04-29 22:59:29 +0100

    Clean after minifying JS

--- gruntfile.js
+++ gruntfile.js
@@ -27,7 +27,7 @@ grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-clean');
 
 // register at least this one task
-grunt.registerTask('default', [ 'uglify' ]);
+grunt.registerTask('default', [ 'uglify', 'clean']);
 
 
 };
