9a1d9d4df35000276b7a305bd7bc08541eeca49a
Ged Lawrenson <gedlawrenson@users.noreply.github.com>
2016-04-29 22:57:18 +0100

    Grunt

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+    // define source files and their destinations
+    uglify: {
+        files: {
+            src: 'js/*.js',  // source files mask
+            dest: 'js/',    // destination folder
+            expand: true,    // allow dynamic building
+            flatten: true,   // remove all unnecessary nesting
+            ext: '.min.js'   // replace .js to .min.js
+        }
+    },
+
+    clean: {
+        js: ['js/*.js', '!js/*.min.js']
+    },
+
+    watch: {
+        js:  { files: 'js/*.js', tasks: [ 'uglify' ] },
+    }
+});
+
+// load plugins
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-clean');
+
+// register at least this one task
+grunt.registerTask('default', [ 'uglify' ]);
+
+
+};
