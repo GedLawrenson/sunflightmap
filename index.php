8d3e59a5ffd1fbcc0c576e26ffbe237e377f5ddb
Ged Lawrenson <gedlawrenson@users.noreply.github.com>
2016-08-26 17:03:02 +0100

    Fixed GA tag

--- index.php
+++ index.php
@@ -827,9 +827,6 @@ if(array_key_exists("autoload", $_GET)) {
 	</div><!-- /header -->
 
 	<div data-role="content" id="home">
-
-			<div id="chrome_note" style="display: none;"><p style="font-size: smaller;">(Please note: This app works best in Google Chrome)</p></div>
-
 			<div data-role="collapsible" data-collapsed="false" id="enter-flight-code">
 	   			<h3><span id="enter-flight-code-title"><?php if($autoload) {?>Loading...<?} else {?>Enter Route Details<? } ?></span></h3>
 
@@ -982,17 +979,14 @@ if(array_key_exists("autoload", $_GET)) {
 
 
 	<!-- google anlaytics -->
-	<script type="text/javascript">
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-76212447-1']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
+	  ga('create', 'UA-27657245-1', 'auto');
+	  ga('send', 'pageview');
 
 	</script>
 	<!-- analytics -->
